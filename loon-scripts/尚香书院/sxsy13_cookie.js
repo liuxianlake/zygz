da823d6a562eb24895bb5111a722913314a02dff
liuxianlake <74122332+liuxianlake@users.noreply.github.com>
2026-08-03 07:23:45 +0800

    Update sxsy13_cookie.js

--- loon-scripts/尚香书院/sxsy13_cookie.js
+++ loon-scripts/尚香书院/sxsy13_cookie.js
@@ -1,1 +1,12 @@
-
+/**
+ * 自动获取 sxsy13 论坛 Cookie
+ */
+if ($request && $request.headers) {
+  const cookie = $request.headers['Cookie'] || $request.headers['cookie'];
+  if (cookie) {
+    if ($persistentStore.write(cookie, "sxsy13_cookie")) {
+      $notification.post("SXSY13 论坛", "Cookie 获取成功 🎉", "已成功保存登录凭证，可以关闭此获取脚本了。");
+    }
+  }
+}
+$done({});
