7f76e932950d09e529be7a14f8d05008632de53f
liuxianlake <74122332+liuxianlake@users.noreply.github.com>
2026-08-06 08:50:10 +0800

    Update sxsy13_checkin.js

--- loonplugin/sxsy/sxsy13_checkin.js
+++ loonplugin/sxsy/sxsy13_checkin.js
@@ -16,7 +16,7 @@ if (!cookie) {
     $done();
 }
 
-const url = "https://sxsy13.com/index.php?mobile=2";
+const url = "https://sxsy13.com/plugin.php?id=k_misign:sign";
 
 const headers = {
     "Cookie": String(cookie),
